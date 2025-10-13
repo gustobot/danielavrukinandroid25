3eb59de22b64cdb7177ea93f13f4db22862dab81
Daniel Avrukin <davrukin@gmail.com>
2025-10-12 23:03:34 -0700

    Updated food item icons

--- app/src/main/java/com/gusto/lunchmenu/data/LunchMenuDataSource.kt
+++ app/src/main/java/com/gusto/lunchmenu/data/LunchMenuDataSource.kt
@@ -10,7 +10,15 @@ class LunchMenuDataSource {
 	/*
 	 * attributions:
 	 * <a href="https://www.flaticon.com/free-icons/waffle" title="waffle icons">Waffle icons created by Freepik - Flaticon</a>
-	 * 
+	 * <a href="https://www.flaticon.com/free-icons/taco" title="taco icons">Taco icons created by vectorsmarket15 - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/curry" title="curry icons">Curry icons created by Freepik - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/pizza" title="pizza icons">Pizza icons created by Vectors Tank - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/sushi" title="sushi icons">Sushi icons created by Freepik - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/breakfast" title="breakfast icons">Breakfast icons created by Konkapp - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/hamburger" title="hamburger icons">Hamburger icons created by Freepik - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/pasta" title="pasta icons">Pasta icons created by Freepik - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/salmon" title="salmon icons">Salmon icons created by Ylivdesign - Flaticon</a>
+	 * <a href="https://www.flaticon.com/free-icons/sandwich" title="sandwich icons">Sandwich icons created by Freepik - Flaticon</a>
 	 */
 
 	private val lunchItems = listOf(
@@ -28,7 +36,7 @@ class LunchMenuDataSource {
 			week = 1,
 			weekday = Weekday.TUESDAY,
 			name = "Tacos",
-			imageResId = R.drawable.chicken, // Replace with a taco image
+			imageResId = R.drawable.tacos,
 			price = 12.50,
 			ingredients = listOf("Tortilla", "Ground Beef", "Lettuce", "Tomato", "Cheese"),
 			allergens = setOf("Gluten", "Dairy")
@@ -37,7 +45,7 @@ class LunchMenuDataSource {
 			week = 1,
 			weekday = Weekday.WEDNESDAY,
 			name = "Vegan Curry",
-			imageResId = R.drawable.chicken, // Replace with a curry image
+			imageResId = R.drawable.curry,
 			price = 13.00,
 			ingredients = listOf("Coconut Milk", "Tofu", "Broccoli", "Peppers", "Rice"),
 			allergens = setOf("Soy"),
@@ -47,7 +55,7 @@ class LunchMenuDataSource {
 			week = 1,
 			weekday = Weekday.THURSDAY,
 			name = "Pizza",
-			imageResId = R.drawable.chicken, // Replace with a pizza image
+			imageResId = R.drawable.pizza,
 			price = 15.50,
 			ingredients = listOf("Dough", "Tomato Sauce", "Cheese", "Pepperoni"),
 			allergens = setOf("Gluten", "Dairy")
@@ -56,7 +64,7 @@ class LunchMenuDataSource {
 			week = 1,
 			weekday = Weekday.FRIDAY,
 			name = "Sushi",
-			imageResId = R.drawable.chicken, // Replace with a sushi image
+			imageResId = R.drawable.sushi,
 			price = 18.00,
 			ingredients = listOf("Rice", "Nori", "Tuna", "Avocado"),
 			allergens = setOf("Fish"),
@@ -66,7 +74,7 @@ class LunchMenuDataSource {
 			week = 2,
 			weekday = Weekday.MONDAY,
 			name = "Breakfast for lunch",
-			imageResId = R.drawable.chicken, // Replace with a breakfast image
+			imageResId = R.drawable.breakfast,
 			price = 11.25,
 			ingredients = listOf("Pancakes", "Sausage", "Scrambled Eggs", "Syrup"),
 			allergens = setOf("Gluten", "Egg", "Dairy")
@@ -75,7 +83,7 @@ class LunchMenuDataSource {
 			week = 2,
 			weekday = Weekday.TUESDAY,
 			name = "Hamburgers",
-			imageResId = R.drawable.chicken, // Replace with a hamburger image
+			imageResId = R.drawable.hamburger,
 			price = 14.00,
 			ingredients = listOf("Bun", "Beef Patty", "Lettuce", "Tomato", "Onion"),
 			allergens = setOf("Gluten")
@@ -84,7 +92,7 @@ class LunchMenuDataSource {
 			week = 2,
 			weekday = Weekday.WEDNESDAY,
 			name = "Spaghetti",
-			imageResId = R.drawable.chicken, // Replace with a spaghetti image
+			imageResId = R.drawable.spaghetti,
 			price = 13.75,
 			ingredients = listOf("Pasta", "Tomato Sauce", "Meatballs", "Parmesan Cheese"),
 			allergens = setOf("Gluten", "Dairy")
@@ -93,7 +101,7 @@ class LunchMenuDataSource {
 			week = 2,
 			weekday = Weekday.THURSDAY,
 			name = "Salmon",
-			imageResId = R.drawable.chicken, // Replace with a salmon image
+			imageResId = R.drawable.salmon,
 			price = 19.50,
 			ingredients = listOf("Salmon Fillet", "Asparagus", "Lemon", "Dill"),
 			allergens = setOf("Fish"),
@@ -103,15 +111,15 @@ class LunchMenuDataSource {
 			week = 2,
 			weekday = Weekday.FRIDAY,
 			name = "Sandwiches",
-			imageResId = R.drawable.chicken, // Replace with a sandwich image
+			imageResId = R.drawable.sandwiches,
 			price = 9.75,
 			ingredients = listOf("Bread", "Turkey", "Lettuce", "Mayonnaise"),
-			allergens = setOf("Gluten", "Egg"),
+			allergens = setOf("Gluten", "Egg")
 		),
 	)
 
 	suspend fun getLunchMenu(): Map<Int, List<FoodItem>> {
-		delay(1_500)
+		delay(1_000)
 		return lunchItems.groupBy { it.week }
 	}
 }
